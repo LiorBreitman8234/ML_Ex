4122be528494fc474e1104d325915355a47f670d
netanel <netanellevine123@gmail.com>
2022-12-29 01:28:34 +0200

    finished adaboost, need to verify results

--- ex3/Adaboost.py
+++ ex3/Adaboost.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 
 def split(data, labels):
-    indexes = np.random.randint(len(data) + 1, len(data) // 2)
+    indexes = np.random.randint(0, len(data) - 1, len(data) // 2)
     train_data = [data[i] for i in indexes]
     test_data = [data[i] for i in range(len(data)) if i not in indexes]
     train_label = [labels[i] for i in indexes]
@@ -18,14 +18,14 @@ def createLines(data):
             if i != j and 'ij' not in dict_lines.keys() and 'ji' not in dict_lines.keys():
                 point_1 = data[i]
                 point_2 = data[j]
-                m = (point_2[1] - point_1[1]) / (point_2[0] - point_1[0])
-                c = point_2[1] - m * point_2[0]
-                dict_lines[f"{i}{j}"] = [m, c]
+                # m = (point_2[1] - point_1[1]) / (point_2[0] - point_1[0])
+                # c = point_2[1] - m * point_2[0]
+                dict_lines[f"{i}{j}"] = (point_1, point_2)
     return dict_lines
 
 
 class Adaboost:
-    def __init__(self, num_iterations):
+    def __init__(self, num_iterations=50):
         self.num_iterations = num_iterations
 
     def __call__(self, data, labels):
@@ -34,3 +34,4 @@ class Adaboost:
         for i in range(self.num_iterations):
             x_train, y_train, x_test, y_test = split(data, labels)
             break
+
